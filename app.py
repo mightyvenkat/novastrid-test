c5a2a100e1d9e2c3c2b43128a8479e30c9816a18
mightyvenkat <149946213+mightyvenkat@users.noreply.github.com>
2025-06-03 13:27:46 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+app.py 
+----------
+from flask import flask
+
+app = flask (venkat)
+
+def hello()
+   return "this is venkat app"
+
+if venkat == "main":
+
+     app.run(host="0.0.0.0,port=8080")
